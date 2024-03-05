d9e3066b9470f787eb1aba03e2d1a9b470887a8e
Kanika <kanikabhatia08sep@gmail.com>
2024-03-05 17:41:57 +0530

    changes

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,15 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const TodoList = () => {
+    const [val, setVal] = useState({
+        name:"",
+        complete: false
+
+    }); //name : input tag
+
+    const [tasks, setTasks] = useState([]) // all the tasks
+
+    console.log(tasks)
+    const addTaskHandler =(event)=>{
+        event.preventDefault();
+        setTasks([...tasks, val]);
+        setVal({...val,name:""});
+    }
+
+
+    const deleteTaskHandler =(index)=>{
+        
+        // let arr = []
+        // tasks.filter(function(task,i) {
+        //     if(i !== index){
+        //         console.log(task,"tasssssskkkkkk iiiiiiii")
+        //         arr.push(task)
+        //     }
+        //     return arr;
+        // });
+        // setTasks(arr)
+        setTasks(tasks.filter((task, i) => i !== index))
+        // console.log(tasks,"taskskkskkskksksksks")
+        // console.log(index,"arrrrrrrrr")
+    }
+
+    const checkTaskHandler = (event,index)=>{
+        const {checked} = event.target;
+        console.log(event.target.checked,"kar");
+        // setTasks(tasks.map((task,i) =>{
+        //     if(i === index){
+        //         task.complete = !checked
+        //     }
+        // }))
+    }
+    
+    const toggleHandler = () =>{
+        
+    }
+
+    function changeHandler(event){
+        setVal( (prevdata) => (
+            {
+                ...prevdata,
+                [event.target.name] : event.target.value
+            }
+        ))
+    }
+
+
     return (
         <div className=''>
             <h1 className=' text-3xl '>TODO List</h1>
-            <input 
-                type='text'
-                name='task'
-                placeholder='Enter a task'
-            /><br/>
-            <button>Add Task</button>
+            <form onSubmit={addTaskHandler}>
+                <input 
+                    type='text'
+                    name='name'
+                    value={val.name}
+                    placeholder='Enter a task'
+                    onChange={changeHandler}
+                /><br/>
+                <button>Add Task</button>
+            
+            </form>
+            {
+                tasks.map((task, index)=>(
+                    <div key={index} className=''>
+                        <input type='checkbox' id='complete' name='complete'  onChange={(e) =>checkTaskHandler(e,index)}/>
+                        <label htmlFor='checked'>{task.name} </label>
+                        <button onClick={toggleHandler}>Edit </button>
+                        <button onClick={()=>deleteTaskHandler(index)}>Delete </button>
+                    </div>
+                ))
+            }
         </div>
+        
     )
 }
