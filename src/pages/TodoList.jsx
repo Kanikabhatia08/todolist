da4e17589d2f79dacbbc8d61783bba9d6b53dbb8
Kanika <kanikabhatia08sep@gmail.com>
2024-03-06 15:34:16 +0530

    changes

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -2,23 +2,27 @@ import React, { useState } from 'react'
 
 export const TodoList = () => {
     const [val, setVal] = useState({
-        name:"",
-        complete: false
+        name: "",
+        complete: false,
 
     }); //name : input tag
+    const [toggle, setToggle] = useState()
+
+    const [save, setSave] = useState('')
+    console.log('toggle', toggle)
 
     const [tasks, setTasks] = useState([]) // all the tasks
 
     console.log(tasks)
-    const addTaskHandler =(event)=>{
+    const addTaskHandler = (event) => {
         event.preventDefault();
         setTasks([...tasks, val]);
-        setVal({...val,name:""});
+        setVal({ ...val, name: "" });
     }
 
 
-    const deleteTaskHandler =(index)=>{
-        
+    const deleteTaskHandler = (index) => {
+
         // let arr = []
         // tasks.filter(function(task,i) {
         //     if(i !== index){
@@ -33,25 +37,25 @@ export const TodoList = () => {
         // console.log(index,"arrrrrrrrr")
     }
 
-    const checkTaskHandler = (event,index)=>{
-        const {checked} = event.target;
-        console.log(event.target.checked,"kar");
-        // setTasks(tasks.map((task,i) =>{
-        //     if(i === index){
-        //         task.complete = !checked
-        //     }
-        // }))
-    }
-    
-    const toggleHandler = () =>{
-        
+    const checkTaskHandler = (event, index) => {
+        const { checked } = event.target;
+        // console.log(event.target.checked,"kar");
+        setTasks(tasks.map((task, i) => {
+            // console.log(i, index, "iiiiiiiii")
+            // console.log(task.complete, checked,"checkkkkkkkkkk")
+            if (i === index) {
+                task.complete = checked
+            }
+            return task;
+        }))
+        console.log(tasks);
     }
 
-    function changeHandler(event){
-        setVal( (prevdata) => (
+    function changeHandler(event) {
+        setVal((prevdata) => (
             {
                 ...prevdata,
-                [event.target.name] : event.target.value
+                [event.target.name]: event.target.value
             }
         ))
     }
@@ -61,27 +65,42 @@ export const TodoList = () => {
         <div className=''>
             <h1 className=' text-3xl '>TODO List</h1>
             <form onSubmit={addTaskHandler}>
-                <input 
+                <input
                     type='text'
                     name='name'
                     value={val.name}
                     placeholder='Enter a task'
                     onChange={changeHandler}
-                /><br/>
+                /><br />
                 <button>Add Task</button>
-            
+
             </form>
             {
-                tasks.map((task, index)=>(
+                tasks.map((task, index) => (
                     <div key={index} className=''>
-                        <input type='checkbox' id='complete' name='complete'  onChange={(e) =>checkTaskHandler(e,index)}/>
+                        <input type='checkbox' id='complete' name='complete' value={task.complete} onChange={(e) => checkTaskHandler(e, index)} />
                         <label htmlFor='checked'>{task.name} </label>
-                        <button onClick={toggleHandler}>Edit </button>
-                        <button onClick={()=>deleteTaskHandler(index)}>Delete </button>
+
+                        {
+                            toggle==index?
+                            "": 
+                            <div>
+                                <button style={{ margin: "20px" }} onClick={() => {setToggle(index)}}>Edit </button>
+                            </div>
+                        }             
+                        {
+                            toggle==index?
+                            <div>
+                                <button style={{ margin: "20px" }} onClick={() => { setToggle() }}>Save </button>
+                            </div>
+                            :''
+                        }
+
+                        <button onClick={() => deleteTaskHandler(index)}>Delete</button>
                     </div>
                 ))
             }
         </div>
-        
+
     )
 }
